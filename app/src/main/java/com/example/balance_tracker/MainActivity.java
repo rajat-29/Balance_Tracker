7b81dd1c04ca1efc8d9a8795426619eec8a5fc00
rajat-29 <rajat295gupta@gmail.com>
2019-06-25 11:44:07 +0530

    interface update

--- app/src/main/java/com/example/balance_tracker/MainActivity.java
+++ app/src/main/java/com/example/balance_tracker/MainActivity.java
@@ -2,12 +2,32 @@ package com.example.balance_tracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tvName,tvBalance;
+    EditText etAmount;
+    Button btnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tvName = findViewById(R.id.tvName);
+        tvBalance = findViewById(R.id.tvBalance);
+        etAmount = findViewById(R.id.etAmount);
+        btnAdd = findViewById(R.id.btnAdd);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
