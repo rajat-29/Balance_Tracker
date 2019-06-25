eee7da4a204796e9bcaf8d7f58344f5db142eb50
rajat-29 <rajat295gupta@gmail.com>
2019-06-25 09:42:38 +0530

    initial commit

---
+++ app/src/main/java/com/example/balance_tracker/UpdateFrag.java
@@ -0,0 +1,29 @@
+package com.example.balance_tracker;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class UpdateFrag extends Fragment {
+
+
+    public UpdateFrag() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_update, container, false);
+    }
+
+}
